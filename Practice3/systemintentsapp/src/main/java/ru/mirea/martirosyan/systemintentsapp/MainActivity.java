8cd6f61a460cd2c70d156bfc2980b23fc2001c43
MorisFromRTU <arme-kara@mail.ru>
2021-05-22 12:59:00 +0300

    Summary

---
+++ Practice3/systemintentsapp/src/main/java/ru/mirea/martirosyan/systemintentsapp/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.mirea.martirosyan.systemintentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickCall(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:89397128854"));
+        startActivity(intent);
+    }
+    public void onClickOpenBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://developer.android.com"));
+        startActivity(intent);
+    }
+    public void onClickOpenMaps(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:55.7934308133998,37.70089684501944"));
+        startActivity(intent);
+    }
+}
